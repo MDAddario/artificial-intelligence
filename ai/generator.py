94dd0df509a83ce3d48833f8968d054d27908ad3
MDAddario <michael.lindner.daddario@gmail.com>
2020-05-24 16:26:02 -0400

    Creates primitive AI

--- ai/generator.py
+++ ai/generator.py
@@ -1,4 +1,22 @@
-filename = "machine_learning.py"
+class AI:
 
-with open(filename, 'w') as file:
-	file.write(f"")
+	filename = "machine_learning.py"
+
+	def __init__(self):
+
+		self.__create_file()
+
+	def __create_file(self):
+
+		with open(self.filename, 'w') as file:
+			pass
+
+	def __println(self, line=""):
+
+		with open(self.filename, 'a') as file:
+			file.write(line + "\n")
+
+
+if __name__ == '__main__':
+
+	ai = AI()
